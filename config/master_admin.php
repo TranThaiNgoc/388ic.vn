1d44c31dd47761f15991ead5d967bfe073843ce2
TranThaiNgoc <ngoctt@vutatech.vn>
2019-05-31 23:03:20 +0700

    no message

---
+++ config/master_admin.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'loaibaiviet' => [
+        'tinnoibat' => 'Tin nổi bật',
+        'topbaiviethay' => 'Top bài viết hay',
+    ],
+	'quyen' => [
+    	'supperadmin' => 'Supper admin',
+    	'admin' => 'Admin',
+    	'user' => 'User',
+    ]
+];
---
+++ resources/views/admin/layout/menu.blade.php
@@ -0,0 +1,55 @@
+<div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="far fa-newspaper"></i> Tin tức<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="{{ route('admin.news') }}">Danh sách tin tức</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.news.add') }}">Thêm tin tức</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-shopping-cart"></i> Đơn hàng<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="#">Danh sách đơn hàng</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-users fa-fw"></i> User<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="{{ route('admin.user') }}">Danh sách User</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.user.add') }}">Thêm User</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
